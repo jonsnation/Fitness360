6d821b9a8064eb4c19cfd671a6988e472ec2b555
Zidane Timothy <zidane.timothy@my.uwi.edu>
2024-04-20 05:08:20 +0000

    TBC

--- App/controllers/workoutroutine.py
+++ App/controllers/workoutroutine.py
@@ -2,8 +2,6 @@ from App.models import WorkoutRoutine
 from App.database import db
 
 def get_all_workout_routines():
-    val = WorkoutRoutine.query.all()
-    print(val)
     return 
 
 def get_workout_routines_json():
